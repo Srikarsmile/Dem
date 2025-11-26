5d3849b3ff3ec3ad12ad4ade22226440280fc470
Srikar_Reddy <srikarsmile10@gmail.com>
2025-11-26 20:04:39 +0530

    server issue fixed

--- server.js
+++ server.js
@@ -5,36 +5,39 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(__dirname));
+app.use(express.static(path.join(process.cwd())));
+app.use('/css', express.static(path.join(process.cwd(), 'css')));
+app.use('/js', express.static(path.join(process.cwd(), 'js')));
+app.use('/images', express.static(path.join(process.cwd(), 'images')));
 
 // Serve index.html for root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(process.cwd(), 'index.html'));
 });
 
 // About page
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'about.html'));
+    res.sendFile(path.join(process.cwd(), 'about.html'));
 });
 
 // Our Story page
 app.get('/our-story', (req, res) => {
-    res.sendFile(path.join(__dirname, 'our-story.html'));
+    res.sendFile(path.join(process.cwd(), 'our-story.html'));
 });
 
 // FAQ page
 app.get('/faq', (req, res) => {
-    res.sendFile(path.join(__dirname, 'faq.html'));
+    res.sendFile(path.join(process.cwd(), 'faq.html'));
 });
 
 // Founder page
 app.get('/founder', (req, res) => {
-    res.sendFile(path.join(__dirname, 'founder.html'));
+    res.sendFile(path.join(process.cwd(), 'founder.html'));
 });
 
 // Contact page
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'contact.html'));
+    res.sendFile(path.join(process.cwd(), 'contact.html'));
 });
 
 // Start server
